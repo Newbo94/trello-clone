5eeb7734218ebe71eb6d212d3f44c0d4b23405e1
Jan-Nilas Gerth <jannilas@hotmail.de>
2019-08-26 22:05:12 +0200

    Testing ts

---
+++ trelloClone/wwwroot/Content/Scripts/Modules/newTaskHandler.ts
@@ -0,0 +1,18 @@
+let newTask = () => {
+    const taskItem: HTMLInputElement = document.querySelector("[data-item]");
+    const itemArr: Array<Object> = [];
+  
+  
+    taskItem.addEventListener("onfocus", e => {
+      const target = <HTMLInputElement>e.currentTarget;
+     
+      const item: Object = new Task(target.value, 1);
+      itemArr.push(item);
+      console.log(itemArr);
+  
+    });
+    
+  };
+  
+  newTask();
+  
---
+++ trelloClone/wwwroot/Content/Scripts/Task/TaskClass.ts
@@ -0,0 +1,10 @@
+class Task {
+  taskTitle: string;
+  taskID: number;
+  isDone: boolean;
+  
+  constructor(taskTitle: string, taskID: number) {
+    this.taskTitle = taskTitle;
+    this.taskID = taskID;
+  }
+}
